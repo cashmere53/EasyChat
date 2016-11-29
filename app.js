549c718eafc724b9d4e0879edc9833944ddda8ea
cashmere53 <ict.nurburgring.mikurusaders53@gmail.com>
2016-11-29 15:04:33 +0900

    rewite all

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-'use strict'
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
 
-const app = require('express')()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
-const PORT = process.env.PORT || 3000
-
-const fs = require('fs')
 app.get(`/`, (req, res) => {
-    // file = fs.readFileSync(__dirname + '/index.html')
-    // res.send(file)
-    fs.readFile(__dirname + '/index.html', 'utf8', (err, data) => {
-        if (err) throw err
-        res.send(data)
-    })
-})
+    res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', (socket) => {
-    console.log('user connected.')
-})
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
 
 http.listen(PORT, () => {
-    console.log(`listen on *:${PORT}`)
-})
-
+    console.log(`listening on *:${PORT}`);
+});
