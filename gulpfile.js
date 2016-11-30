d301ac7bf77ff4ed453f8e27366cee06f71d2dc2
cashmere53 <ict.nurburgring.mikurusaders53@gmail.com>
2016-12-01 02:17:06 +0900

    add uglify task

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,31 @@
 var gulp = require('gulp')
 var pug = require('gulp-pug')
 var stylus = require('gulp-stylus')
+var uglify = require('gulp-uglify')
 var del = require('del')
 
 var path = {
-    ouput: __dirname + '/outpot'
+    dist: './output/',
+    src: './view/'
 }
 
 gulp.task('default', () => {
 
 })
 
+gulp.task('uglify', () => {
+    console.log('--- uglify ---')
+    gulp.src(path['src'] + '*.js')
+        .pipe(uglify({
+            'preserveComments': 'some'
+        }))
+        .pipe(gulp.dest(path['dist']))
+})
+
+gulp.task('watch', () => {
+    var w_uglify = gulp.watch(path['src'] + '*.js', ['uglify'])
+
+    w_uglify.on('change', (event) => {
+        console.log('javascript File ' + event.path + ' was ' + event.type + ', running task uglify...')
+    })
+})
