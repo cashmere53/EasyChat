c5166e7b8c9aa094ec17d07be748fd2c8a3ea96c
cashmere53 <ict.nurburgring.mikurusaders53@gmail.com>
2016-12-01 03:00:54 +0900

    add pug, stylus and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp')
 var pug = require('gulp-pug')
 var stylus = require('gulp-stylus')
 var uglify = require('gulp-uglify')
+var plumber = require('gulp-plumber')
+var notify = require('gulp-notify')
 var del = require('del')
 
 var path = {
@@ -11,13 +13,38 @@ var path = {
     src: './view/'
 }
 
-gulp.task('default', () => {
+gulp.task('default', ['watch'])
 
+gulp.task('pug', () => {
+    console.log('--- pug ---')
+    gulp.src(path['src'] + '*.pug')
+        .pipe(plumber({
+            'errorHandler': notify.onError("Error: <%= error.message %>")
+        }))
+        .pipe(pug({
+            'pretty': true
+        }))
+        .pipe(gulp.dest(path['dist']))
+})
+
+gulp.task('stylus', () => {
+    console.log('--- stylus ---')
+    gulp.src(path['src'] + '*.styl')
+        .pipe(plumber({
+            'errorHandler': notify.onError("Error: <%= error.message %>")
+        }))
+        .pipe(stylus({
+            'compress': true
+        }))
+        .pipe(gulp.dest(path['dist']))
 })
 
 gulp.task('uglify', () => {
     console.log('--- uglify ---')
     gulp.src(path['src'] + '*.js')
+        .pipe(plumber({
+            'errorHandler': notify.onError("Error: <%= error.message %>")
+        }))
         .pipe(uglify({
             'preserveComments': 'some'
         }))
@@ -25,8 +52,18 @@ gulp.task('uglify', () => {
 })
 
 gulp.task('watch', () => {
+    console.log('start watching.')
+
+    var w_pug = gulp.watch(path['src'] + '.pug', ['pug'])
+    var w_stylus = gulp.watch(path['src'] + '*.styl', ['stylus'])
     var w_uglify = gulp.watch(path['src'] + '*.js', ['uglify'])
 
+    w_pug.on('change', (event) => {
+        console.log('pug File ' + event.path + ' was ' + event.type + ', running task uglify...')
+    })
+    w_stylus.on('change', (event) => {
+        console.log('stylus File ' + event.path + ' was ' + event.type + ', running task uglify...')
+    })
     w_uglify.on('change', (event) => {
         console.log('javascript File ' + event.path + ' was ' + event.type + ', running task uglify...')
     })
