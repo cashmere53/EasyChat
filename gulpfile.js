65a4b4da2a64f33899680ccd40bb4cf44e11f4e3
cashmere53 <ict.nurburgring.mikurusaders53@gmail.com>
2016-12-01 01:59:45 +0900

    add gulp, do nothing yet.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var gulp = require('gulp')
+var pug = require('gulp-pug')
+var stylus = require('gulp-stylus')
+var del = require('del')
+
+var path = {
+    ouput: __dirname + '/outpot'
+}
+
+gulp.task('default', () => {
+
+})
+
